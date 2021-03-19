0ae07759e3c881597dce60ce358990ee31f737d1
metacoder87 <gary.miller@gmx.com>
2021-03-19 10:11:20 -0400

    Adds peeks

--- stack.rb
+++ stack.rb
@@ -12,3 +12,8 @@ class Stack
     def pop
       @ivar.shift # removes one element from the stack
     end
+
+    def peek
+      @ivar.first # returns, but doesn't remove, the top element in the stack
+    end
+  end
