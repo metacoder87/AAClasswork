16b3a4ea463404220d1dc0b48140c8710cea4b41
metacoder87 <gary.miller@gmx.com>
2021-03-29 14:27:38 -0400

    Adds new_move_positions and build_move_tree

--- gary_miller_knights_travails/knight_path_finder.rb
+++ gary_miller_knights_travails/knight_path_finder.rb
@@ -10,6 +10,15 @@ class KnightPathFinder
     def initialize(pos)
         @considered = [pos]
         @starting_position = pos
+        build_move_tree
+    end
+
+    def new_move_positions(pos)
+        self.valid_moves !include?(@considered)
+    end
+
+    def build_move_tree(pos)
+
     end
 
 end
