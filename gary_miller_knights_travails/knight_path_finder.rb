ad44aaa3d9a92033498e261e940864c6630c3080
metacoder87 <gary.miller@gmx.com>
2021-03-29 14:21:17 -0400

    Adds new file and valid_moves method

---
+++ gary_miller_knights_travails/knight_path_finder.rb
@@ -0,0 +1,15 @@
+
+
+
+class KnightPathFinder
+
+    def self.valid_moves(pos)
+        self.parent
+    end
+
+    def initialize(pos)
+        @considered = [pos]
+        @starting_position = pos
+    end
+
+end
