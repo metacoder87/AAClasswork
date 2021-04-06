bc9c5b992bc91c3e09c1c4ad813d9470ee7f8f02
metacoder87 <gary.miller@gmx.com>
2021-04-06 04:19:03 -0400

    Working on move for super_comp_player

--- gary_miller_Tic-Tac-Toe_AI/lib/super_computer_player.rb
+++ gary_miller_Tic-Tac-Toe_AI/lib/super_computer_player.rb
@@ -2,6 +2,9 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = Tic_Tac_Toe_Node.new(game, mark)
+    winner = node.children.select { |child| node.winning_node?(mark) }
+    return winner.prev_move_pos
   end
 end
 
