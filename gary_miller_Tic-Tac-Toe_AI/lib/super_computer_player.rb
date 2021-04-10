b95e474b8199d79c0af2fe18a4258453545cd090
metacoder87 <gary.miller@gmx.com>
2021-04-10 11:50:30 -0400

    Still working on the move

--- gary_miller_Tic-Tac-Toe_AI/lib/super_computer_player.rb
+++ gary_miller_Tic-Tac-Toe_AI/lib/super_computer_player.rb
@@ -1,17 +1,21 @@
 require_relative 'tic_tac_toe_node'
+require 'byebug'
 
 class SuperComputerPlayer < ComputerPlayer
 
   def move(game, mark)
-    moves = []
-    node = TicTacToeNode.new(game.board, mark)
-    if node.children.none? { |child| child.winning_node?(mark) }
-        return node.children.select { |child| !child.losing_node?(mark) ? child.prev_move_pos : next }.first
-    else node.children.each { |child| moves << child.prev_move_pos if child.winning_node?(mark) }
-    end
-    moves
+    @node = TicTacToeNode.new(game.board, mark)
+    not_losing_node(mark)
   end
-  
+
+  def winning_node(mark)
+    @node.children.select { |child| child.winning_node?(mark) }.first.prev_move_pos 
+  end
+
+  def not_losing_node(mark)
+    @node.children.select { |child| child.losing_node?(child.prev_mark) ? next : child }
+  end
+
 end
 
 if __FILE__ == $PROGRAM_NAME
