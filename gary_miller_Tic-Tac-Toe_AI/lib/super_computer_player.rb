f50ef0dc4bf1e36232b17fb274c5129ba959d65d
metacoder87 <gary.miller@gmx.com>
2021-04-10 21:47:42 -0400

    Finally passed two of the last rspec tests

--- gary_miller_Tic-Tac-Toe_AI/lib/super_computer_player.rb
+++ gary_miller_Tic-Tac-Toe_AI/lib/super_computer_player.rb
@@ -3,19 +3,22 @@ require_relative 'tic_tac_toe_node'
 class SuperComputerPlayer < ComputerPlayer
 
   def move(game, mark)
-    @node = TicTacToeNode.new(game.board, mark)
-    if winning_node(mark).nil?
-      return not_losing_node(mark)
-    end
-    return winning_node(mark)
+    node = TicTacToeNode.new(game.board, mark)
+    @moves = node.children
+
+    node = winning_node(mark)
+    return node.prev_move_pos if node
+
+    node = not_losing_node(mark)
+    return node.prev_move_pos if node
   end
 
   def winning_node(mark)
-    @node.children.select { |child| child.winning_node?(mark) }.first.prev_move_pos 
+    @moves.find { |child| child.winning_node?(mark) }
   end
 
   def not_losing_node(mark)
-    @node.children.select { |child| child.losing_node?(child.prev_mark) ? next : child }.first.prev_move_pos
+    @moves.find { |child| !child.losing_node?(mark) }
   end
 
 end
