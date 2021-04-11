40cdd388fa28c19becd8badefd43fca63c628d29
metacoder87 <gary.miller@gmx.com>
2021-04-10 22:11:25 -0400

    Adds commenting.

--- gary_miller_Tic-Tac-Toe_AI/lib/super_computer_player.rb
+++ gary_miller_Tic-Tac-Toe_AI/lib/super_computer_player.rb
@@ -2,16 +2,19 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
 
+    # Finds the best possible move each time to always WIN or at least prevent
+    # the other player from winning.
   def move(game, mark)
+    # Generates all possible move nodes
     node = TicTacToeNode.new(game.board, mark)
     moves = node.children
-
+    # Finds the winning node if it exists.
     node = moves.find { |child| child.winning_node?(mark) }
     return node.prev_move_pos if node
-
+    # Otherwise it finds the node that won't lose.
     node = moves.find { |child| !child.losing_node?(mark) }
     return node.prev_move_pos if node
-
+    # Otherwise it admits defeat.
     raise "No winning path found. You will lose."
   end
 
