9fb8240672d965ed09751cd4e866d46e8ef1a794
metacoder87 <gary.miller@gmx.com>
2021-04-20 20:30:29 -0400

    Adds manager level bonus

--- gary_miller_class_inheritance/manager.rb
+++ gary_miller_class_inheritance/manager.rb
@@ -5,9 +5,15 @@ require_relative 'employee'
 
 class Manager < Employee
 
+    attr_reader :employees
+
     def initialize(name, title, salary, boss, employees)
         @employees = employees
         super(name, title, salary, boss)
+    end
+
+    def bonus(multiplier)
+        return @employees.map { |worker| worker.salary }.inject(:+) * multiplier
     end
 
 end
