7206d33dd11a3406a80da9625ccea3e4ae82c21c
metacoder87 <gary.miller@gmx.com>
2021-04-20 21:20:22 -0400

    Adds the class creation and bonus tests

--- gary_miller_class_inheritance/manager.rb
+++ gary_miller_class_inheritance/manager.rb
@@ -17,3 +17,12 @@ class Manager < Employee
     end
 
 end
+
+david = Employee.new("David", "TA", 10000, "Darren")
+shawna = Employee.new("Shawna", "TA", 12000, "Darren")
+darren = Manager.new("Darren", "TA Manager", 78000, "Ned", [shawna, david])
+ned = Manager.new("Ned", "Founder", 1000000, nil, [darren, shawna, david])
+
+ned.bonus(5) # => 500_000
+darren.bonus(4) # => 88_000
+david.bonus(3) # => 30_000
