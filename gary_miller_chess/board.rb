1d7a197c9635f01aa661f11ea6edd1f55c05c517
metacoder87 <gary.miller@gmx.com>
2021-04-22 06:53:16 -0400

    Adds a populate helper and fixes everything else

--- gary_miller_chess/board.rb
+++ gary_miller_chess/board.rb
@@ -2,10 +2,18 @@ require_relative 'piece'
 
 class Board
 
-    attr_reader :grid
+    attr_reader :rows
 
     def initialize
-        @grid = Array.new(8) { [Piece.new] }
+        @rows = Array.new(8) { Array.new(8) }
+        populate
+    end
+
+    def populate
+        @rows.each_with_index do |row, idx|
+            row.map!.with_index { |col, i| col = Piece.new([idx, i]) } if idx == 0 || idx == 1 || idx == 6 || idx == 7 
+        end
+    end
     end
 
 end
