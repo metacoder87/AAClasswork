420280f4bb99a5b35a6b42a2e3940858ad8c7127
metacoder87 <gary.miller@gmx.com>
2021-04-30 01:48:31 -0400

    Adds some fixes

--- gary_miller_chess/board.rb
+++ gary_miller_chess/board.rb
@@ -10,25 +10,63 @@ class Board
     end
 
     def populate
+        board = @rows.dup
         @rows.each_with_index do |row, idx|
             row.map!.with_index do |col, i| 
-                if idx == 0 || idx == 1 
-                    col = Piece.new("black", @rows, [idx, i]) 
-                elsif idx == 6 || idx == 7 
-                    col = Piece.new("white", @rows, [idx, i])
+                if idx == 0
+                    if i == 0 || i == 7
+                        col = Piece.new(:BR, board, [idx, i])
+                    elsif i == 1 || i == 6
+                        col = Piece.new(:Bk, board, [idx, i])
+                    elsif i == 2 || i == 5
+                        col = Piece.new(:BB, board, [idx, i])
+                    elsif i == 3 
+                        col = Piece.new(:BQ, board, [idx, i])
+                    else col = Piece.new(:BK, board, [idx, i])
+                    end
+                elsif idx == 1
+                    col = Piece.new(:BP, board, [idx, i])
+                elsif idx == 6
+                    col = Piece.new(:WP, board, [idx, i])
+                elsif idx == 7
+                    if i == 0 || i == 7
+                        col = Piece.new(:WR, board, [idx, i])
+                    elsif i == 1 || i == 6
+                        col = Piece.new(:Wk, board, [idx, i])
+                    elsif i == 2 || i == 5
+                        col = Piece.new(:WB, board, [idx, i])
+                    elsif i == 3
+                        col = Piece.new(:WQ, board, [idx, i])
+                    else col = Piece.new(:WK, board, [idx, i])
+                    end
                 end
             end
         end
     end
 
-    def [](pos)
-        x, y = pos
-        @rows[x][y]
+    def [](pos_x, pos_y)
+        x, y = pos_x, pos_y
+        @rows[x][y].color
     end
 
-    def []=(pos, val)
-        x, y = pos
-        @rows[x][y]
+    def []=(pos_x, pos_y, val)
+        x, y = pos_x, pos_y
+        @rows[x][y].position = val
+    end
+
+    def render
+        grid = []
+        @rows.each do |row|
+            arr = []
+            row.each do |col|
+                    if col
+                        arr << col.color
+                    else arr << col
+                    end
+                end
+                grid << arr
+            end
+        grid.each { |row| puts row.join(" ") }
     end
 
     def move_piece(start_pos, end_pos)
