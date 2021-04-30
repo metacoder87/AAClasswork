f035623111ad2586b738167f7844a97d68c7bbe6
metacoder87 <gary.miller@gmx.com>
2021-04-30 01:49:04 -0400

    Filling in requested methods

--- gary_miller_chess/piece.rb
+++ gary_miller_chess/piece.rb
@@ -6,5 +6,29 @@ class Piece
         @board, @color, @position = board, color, position
     end
 
+    def to_s
+
+    end
+
+    def empty?
+        
+    end
+
+    def valid_moves
+
+    end
+
+    def pos=(val)
+
+    end
+
+    def Symbol
+
+    end
+
+    def move_into_check?(end_pos)
+        
+    end
+
 end
 
