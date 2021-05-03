25fcf3d2b652737a9f761d363b67a5768460b459
metacoder87 <gary.miller@gmx.com>
2021-05-02 22:44:45 -0400

    Still working on the various pieces

--- gary_miller_chess/pawn.rb
+++ gary_miller_chess/pawn.rb
@@ -4,11 +4,18 @@
 class Pawn < Piece
 
     def symbol
-        return :P 
+        return :BP if @position.first == 1
+        return :WP 
     end
 
     def moves
-
+        x, y = @position
+        mov = []
+        mov << [x + forward_dir, y] if @color == :WP
+        mov << [x + 1, y] if @color == :BP
+        mov << [x + 2, y] if @color == :BP && x == 1
+        mov << [x - 2, y] if @color == :WP && x == 6
+        return mov   
     end
 
 private
@@ -18,11 +25,14 @@ private
     end
 
     def forward_dir # => returns 1 || -1
-
+        return -1 if @color == :WP
+        return 1 if @color == :BP
     end
 
     def forward_steps
-
+        x, y = @position
+        return 2 if x == 1 && @color == :BP
+        return 2 if x ==  
     end
 
     def side_attacks
--- gary_miller_chess/piece.rb
+++ gary_miller_chess/piece.rb
@@ -3,31 +3,34 @@ class Piece
     attr_accessor :position, :board, :color
 
     def initialize(color, board, position)
-        @board, @color, @position = board, color, position
+        @board, @color, @position = board, symbol, position
     end
 
     def moves
-
+        # Each subclass moves differently.
     end
 
     def to_s
-
+        @color.to_s
     end
 
     def empty?
-        
+        @color == :_
     end
 
     def valid_moves
-
+        moves.select do |move|
+            x, y = move 
+            @board[x, y].empty?
+        end
     end
 
     def pos=(val)
-
+        @position = val
     end
 
-    def Symbol
-
+    def symbol
+        return :_
     end
 
     def move_into_check?(end_pos)
