9cc3eee854d118b2752d5152ac03c426153ded6f
metacoder87 <gary.miller@gmx.com>
2021-05-04 19:36:48 -0400

    Wrote Pawn ready for testing

--- gary_miller_chess/pawn.rb
+++ gary_miller_chess/pawn.rb
@@ -21,7 +21,9 @@ class Pawn < Piece
 private
 
     def at_start_row?
-
+        return true if @color == :BP && @position.first == 1
+        return true if @color == :WP && @position.first == 6
+        false
     end
 
     def forward_dir # => returns 1 || -1
@@ -30,13 +32,22 @@ private
     end
 
     def forward_steps
-        x, y = @position
-        return 2 if x == 1 && @color == :BP
-        return 2 if x ==  
+        return 2 if at_start_row?
+        return 1
     end
 
     def side_attacks
-
+        attacks = []
+        x, y = @position
+        x2 = x += forward_dir
+        l_attack = [x2, y - 1]
+        r_attack = [x2, y + 1]
+        if !@board.l_attack.empty? && @board.l_attack.color != self.color
+            attacks << l_attack
+        elsif !@board.r_attack.empty? && @board.r_attack.color != self.color
+            attacks << r_attack
+        end
+        return attacks
     end
 
 end
