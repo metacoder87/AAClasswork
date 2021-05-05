9de3f8f407725321d6f799dab3a7504ac85e0349
metacoder87 <gary.miller@gmx.com>
2021-05-04 20:18:08 -0400

    Ready to test Knight

--- gary_miller_chess/knight.rb
+++ gary_miller_chess/knight.rb
@@ -5,13 +5,14 @@ class Knight < Piece
     include Stepable
 
     def symbol
-        return :Ni
+        return :BN if @position.first == 0
+        return :BK
     end
 
 protected
 
     def move_difs
-        
+
     end
 
 end
