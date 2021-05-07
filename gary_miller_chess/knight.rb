987fd7b49376b387ffab52fc5fce6a7abb694508
metacoder87 <gary.miller@gmx.com>
2021-05-07 00:38:31 -0400

    Adds a move finder for knight

--- gary_miller_chess/knight.rb
+++ gary_miller_chess/knight.rb
@@ -18,7 +18,14 @@ class Knight < Piece
 protected
 
     def move_difs
-
+        valid_moves = []
+        knight_moves = [[1,2],[2,1],[-1,2],[-2,1],[1,-2],[2,-1],[-1,-2],[-2,-1]]
+        knight_moves.each do |move|
+            x = pos[0] + move[0]
+            y = pos[1] + move[1] 
+            valid_moves << [x,y] if (0..7).include?(x) && (0..7).include?(y)
+        end
+        valid_moves
     end
 
 end
