7007dc48de813c201b6e3a8fe5639405e14ca08b
metacoder87 <gary.miller@gmx.com>
2021-05-19 06:32:45 -0400

    Updates Bishop

--- gary_miller_chess/bishop.rb
+++ gary_miller_chess/bishop.rb
@@ -4,11 +4,6 @@ require_relative 'slideable'
 class Bishop < Piece
     include Slideable
 
-    def initialize(board, position)
-        super
-        @color = symbol
-    end
-
     def symbol
         x, y = @position
         return :♝B if x == 0
