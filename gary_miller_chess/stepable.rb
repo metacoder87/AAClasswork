e9bfbfc8861f9ba7eeaf3e77bb0c91e81dd5ba05
metacoder87 <gary.miller@gmx.com>
2021-06-16 00:12:43 -0400

    Adds functionality to Stepable mod

--- gary_miller_chess/stepable.rb
+++ gary_miller_chess/stepable.rb
@@ -1,13 +1,25 @@
 module Stepable
 
     def moves
-
+        all_moves = []
+        move_diffs.each do |move|
+            x, y = @position
+            x1, y1 = move
+                x += x1 && y += y1
+                all_moves << [x,y] if (0..7).include?(x) && (0..7).include?(y)
+        end
+        return all_moves
     end
 
 private
 
-    def move_diffs
+    SINGLE_STEPS = [[0,1], [0,-1], [1,0], [-1,0], [1,1], [1,-1], [-1,-1], [-1,1]]
 
+    LONG_STEPS = [[1,-2], [2,-1],  [2,1],  [1,2], [-1,-2] [-2,-1], [-2,1], [-1,2]]
+
+    def move_diffs
+        return LONG_STEPS if self.is_a?(Knight)
+        return SINGLE_STEPS if self.is_a?(King)
     end
 
 end
