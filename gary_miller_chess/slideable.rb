64c771d4032e583c207d5bd0d1721f138b930126
metacoder87 <gary.miller@gmx.com>
2021-06-23 04:56:59 -0400

    wip on grow_unblocked_moves_in_dir

--- gary_miller_chess/slideable.rb
+++ gary_miller_chess/slideable.rb
@@ -1,4 +1,4 @@
-
+require 'byebug'
 module Slideable
 
     def horizontal_dirs
@@ -15,7 +15,9 @@ module Slideable
         all_moves = []
         move_dirs.each do |move|
             x, y = move
-            all_moves << grow_unblocked_moves_in_dir(x, y)
+            grow_unblocked_moves_in_dir(x, y).each do |spot|
+                all_moves << spot
+            end
         end
         return all_moves
     end
@@ -28,26 +30,42 @@ module Slideable
 
     def move_dirs
         both = []
-        both << diaganol_dirs && horizontal_dirs
         if self.is_a?(Bishop)
             return diaganol_dirs 
         elsif self.is_a?(Rook)
             return horizontal_dirs 
-        else return both
+        else 
+            both << diaganol_dirs
+            both << horizontal_dirs
+            return both
         end
+    end
+
+    def opposite_color(colour)
+        return true if @color == "white" && colour == "black"
+        return true if @color == "black" && colour == "white"
+        return false
     end
 
     def grow_unblocked_moves_in_dir(dx, dy)
         clear_path = []
         x, y = @position
-        while (0..7).to_a.include?(x) && (0..7).to_a.include?(y)
+        #debugger
+        until !(0..7).include?(x) || !(0..7).include?(y)
             x += dx
             y += dy
-            if @board
-                clear_path << [x, y] 
-            end
+            if (0..7).include?(x) && (0..7).include?(y) && board[x][y].symbol == :__
+                clear_path << [x,y]
+            elsif (0..7).include?(x) && (0..7).include?(y) && board[x][y].color == opposite_color(self.color)
+                clear_path << [x,y]
+                break
+            elsif (0..7).include?(x) && (0..7).include?(y) && board[x][y].color == self.color
         end
+        
         return clear_path
     end
 
 end
+
+# board.move_piece([1,0],[2,1])
+# board[0,0].grow_unblocked_moves_in_dir(1,0)
