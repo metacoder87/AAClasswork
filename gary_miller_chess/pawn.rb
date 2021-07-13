ceb394b08cb98d9fa3d16e823d715cb6d42cc627
metacoder87 <gary.miller@gmx.com>
2021-07-13 01:36:05 -0400

    Unicode refactor and color fix pawn

--- gary_miller_chess/pawn.rb
+++ gary_miller_chess/pawn.rb
@@ -8,39 +8,31 @@ class Pawn < Piece
         @color = color || set_color
     end
 
-    def set_color
-        x, y = @position
-        return "black" if x == 0 || x == 1
-        return "white" if x == 6 || x == 7
-    end
-
     def symbol
-        x, y = @position
-        return :♟p if x == 1
-        return :♙p 
+        return "\u265f"+"p"
     end
 
     def moves
         x, y = @position
         mov = []
-        mov << [x + forward_dir, y] if @color == "white"
-        mov << [x + 1, y] if @color == "black"
-        mov << [x + 2, y] if @color == "black" && x == 1
-        mov << [x - 2, y] if @color == "white" && x == 6
+        mov << [x + forward_dir, y] if @color == "black"
+        mov << [x + 1, y] if @color == "white"
+        mov << [x + 2, y] if @color == "white" && x == 1
+        mov << [x - 2, y] if @color == "black" && x == 6
         return mov   
     end
 
 private
 
     def at_start_row?
-        return true if @color == "black" && @position.first == 1
-        return true if @color == "white" && @position.first == 6
+        return true if @color == "white" && @position.first == 1
+        return true if @color == "black" && @position.first == 6
         false
     end
 
     def forward_dir # => returns 1 || -1
-        return -1 if @color == "white"
-        return 1 if @color == "black"
+        return -1 if @color == "black"
+        return 1 if @color == "white"
     end
 
     def forward_steps
