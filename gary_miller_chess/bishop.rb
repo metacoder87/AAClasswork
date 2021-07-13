13ad48afb8245947536867cc1dc939d019dbf151
metacoder87 <gary.miller@gmx.com>
2021-07-13 01:36:56 -0400

    Unicode refactor bishop

--- gary_miller_chess/bishop.rb
+++ gary_miller_chess/bishop.rb
@@ -9,16 +9,8 @@ class Bishop < Piece
         @color = color || set_color
     end
 
-    def set_color
-        x, y = @position
-        return "black" if x == 0 || x == 1
-        return "white" if x == 6 || x == 7
-    end
-
     def symbol
-        x, y = @position
-        return :♝B if x == 0
-        return :♗B
+        return "\u265d"+"B"
     end
 
 private
