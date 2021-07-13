0310d9803b3e002b414fd25070b0ac5332f129aa
metacoder87 <gary.miller@gmx.com>
2021-07-13 01:35:34 -0400

    Unicode refactor queen

--- gary_miller_chess/queen.rb
+++ gary_miller_chess/queen.rb
@@ -9,16 +9,8 @@ class Queen < Piece
         @color = color || set_color
     end
 
-    def set_color
-        x, y = @position
-        return "black" if x == 0 || x == 1
-        return "white" if x == 6 || x == 7
-    end
-
     def symbol
-        x, y = @position
-        return :♛Q if x == 0
-        return :♕Q
+        return "\u265b"+"Q"
     end
 
 private
