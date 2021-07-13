08bc2ee73aeb591932a53d87ad47f31027cec3d8
metacoder87 <gary.miller@gmx.com>
2021-07-13 01:36:42 -0400

    Unicode refactor king

--- gary_miller_chess/king.rb
+++ gary_miller_chess/king.rb
@@ -8,17 +8,9 @@ class King < Piece
         super
         @color = color || set_color
     end
-
-    def set_color
-        x, y = @position
-        return "black" if x == 0 || x == 1
-        return "white" if x == 6 || x == 7
-    end
-
+    
     def symbol
-        x, y = @position
-        return :♚K if x == 0
-        return :♔K
+        return "\u265a"+"K"
     end
 
 protected
