9173e4c5a4b417e114993d68fe8acc7b0826d7b9
metacoder87 <gary.miller@gmx.com>
2021-07-30 08:17:36 -0400

    Adds commenting to Bishop

--- gary_miller_chess/bishop.rb
+++ gary_miller_chess/bishop.rb
@@ -1,21 +1,21 @@
 require_relative 'slideable'
 
-
+# Creates the Bishop class and includes superclass Piece
 class Bishop < Piece
-    include Slideable
+    include Slideable # Module containing the move logic
 
     def initialize(board, position, color = nil)
-        super
+        super # Gets board & position and calls set_color from Piece
         @color = color || set_color
     end
 
-    def symbol
+    def symbol # Uses Unicode to create a bishop chess piece
         return "\u265d"+"B"
     end
 
 private
 
-    def move_dirs
+    def move_dirs # The bishop piece always moves in the diaganol direction
         diaganol_dirs
     end
 
