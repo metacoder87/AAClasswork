f9b1febc6d0788d4d4d6ef1eb646ab1d8841db97
metacoder87 <gary.miller@gmx.com>
2021-09-06 06:26:31 -0400

    Adds some more rspec tests

--- gary_miller_first_tdd_projects/spec/tdd_projects_spec.rb
+++ gary_miller_first_tdd_projects/spec/tdd_projects_spec.rb
@@ -3,7 +3,7 @@ require 'rspec'
 describe 'my_uniq' do
 
     it 'works on the given test to return an array of unique elements' do
-        expect([1, 2, 1, 3, 3].uniq).to eq([1, 2, 3])
+        expect([1, 2, 1, 3, 3].my_uniq).to eq([1, 2, 3])
     end
     it 'makes an arrays elements unique' do
         expect([1,1,1,1,1,2,3,3,3,3,3,3,3,4,4,4,4,5].my_uniq).to eq([1,1,1,1,1,2,3,3,3,3,3,3,3,4,4,4,4,5].uniq)
@@ -21,10 +21,10 @@ describe 'my_uniq' do
         expect([513549876513,654654687984654,654651213541687,513549876513,654651213541687,987948531546897,513549876513,654651213541687,6549841354987651].my_uniq).to eq([513549876513,654654687984654,654651213541687,513549876513,654651213541687,987948531546897,513549876513,654651213541687,6549841354987651].uniq)
     end
     it 'returns elements in the order in which they were given' do
-        
+        expect([7,16,1,7,7,7,1,1,1].my_uniq).to eq([7,16,1])
     end
     it 'throws an error if used on anything other than an array' do
-        
+        expect('not an array'.my_uniq).to raise_error(TypeError)
     end
 
 end
@@ -35,16 +35,16 @@ describe 'two_sum' do
         expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
     end
     it 'finds all pairs of positions where the elements at those positions sum to zero' do
-        expect([3,-6,0,5,78,55,6,-78]).to eq([[1,6],[4,7]])
+        expect([3,-6,0,5,78,55,6,-78].two_sum).to eq([[1,6],[4,7]])
     end
     it 'pairs to be sorted smaller index before bigger index' do
-        
+        expect([1,2,3,-2,3,-1].two_sum).to eq([[0,5],[1,3]])
     end
     it 'pairs to be sorted "dictionary-wise"' do
-        
+        expect([-99,99,-1,2,1,-16,4,16].two_sum).to eq([[0,1],[2,4],[5,7]])
     end
     it 'smaller first elements come first, then smaller second elements come first' do
-        
+        expect([-3,9,7,-9,3].two_sum).to eq([[0,4],[1,3]])
     end
 
 end
@@ -54,12 +54,19 @@ describe 'my_transpose' do
     it 'passes the given test case' do
         expect(my_transpose([[0, 1, 2],
                              [3, 4, 5],
-                             [6, 7, 8]]).to eq([[0, 3, 6],
+                             [6, 7, 8]])).to eq([[0, 3, 6],
                                                 [1, 4, 7],
-                                                [2, 5, 8]]
+                                                [2, 5, 8]])
     end
     it 'converts between the row-oriented and column-oriented representations' do
-        
+        expect(my_transpose([['a','b','c'],
+                             ['d','e','f'],
+                             ['g','h','i']])).to eq([['a','d','g'],
+                                                     ['b','e','h'],
+                                                     ['c','f','i']])
+    end
+    it 'throws an error when given anything but an array of arrays' do
+        expect { my_transpose(['not an array of arrays']) }.to raise_error(TypeError)
     end
 end
 
