4238c10fbfa99515a01774f35d7d8d2213eff747
metacoder87 <gary.miller@gmx.com>
2021-09-12 01:36:47 -0400

    A few more classes to test

--- gary_miller_poker/spec/poker_spec.rb
+++ gary_miller_poker/spec/poker_spec.rb
@@ -1,8 +1,32 @@
 require 'rspec'
 require 'poker'
 
+describe Card do
+    let(:card) do
+        Card.new
+    end
+end
+
+describe Deck do
+    let(:deck) do
+        Deck.new
+    end
+end
+
 describe Hand do
     let(:hand) do
         Hand.new()
+    end
+end
+
+describe Player do
+    let(:player) do
+        Player.new
+    end
+end
+
+describe Game do
+    let(:game) do
+        Game.new
     end
 end
