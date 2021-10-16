777f4b5b2727d8a16205fa75047b217bb4d1d22a
metacoder87 <gary.miller@gmx.com>
2021-10-16 09:04:02 -0400

    Adds comments for future understanding

--- gary_miller_poker/lib/poker.rb
+++ gary_miller_poker/lib/poker.rb
@@ -38,7 +38,7 @@ require 'byebug'
 # Classes you will want:
 # Card
 
-class Card
+class Card # This class represents each individual card in the deck
 
     attr_reader :suit, :value
 
@@ -54,17 +54,17 @@ class Deck
 
     attr_reader :cards
 
-    def initialize
+    def initialize # Creates a deck of cards
         @cards = deck_builder
     end
 
-    def value_array
+    def value_array # An array of all possible values in a deck
         base_vals = [2,3,4,5,6,7,8,9,10]
         face_vals = [:Jack,:Queen,:King,:Ace]
         return base_vals + face_vals
     end
 
-    def suit_hasher
+    def suit_hasher # A hash table of all the suits in a deck
         suits = [:hearts,:clubs,:diamonds,:spaids]
         cards = {}
         suits.each do |suit|
@@ -73,7 +73,7 @@ class Deck
         return cards
     end
 
-    def deck_builder
+    def deck_builder # Fills in the hash table so the each suit holds all values
         cards = suit_hasher
         cards.keys.each do |suit|
             value_array.each do |val|
@@ -95,12 +95,12 @@ class Hand
 
     attr_accessor :cards, :values
 
-    def initialize(cards = [])
-        @cards = cards
-        @values = []
+    def initialize(cards = []) 
+        @cards = cards # Holds all the cards for a players hand
+        @values = [] # Tracks a numerical value of each card in hand
     end
 
-    def sort_vals
+    def sort_vals # Assigns a numerical value for each card
         @values.clear
         @cards.each do |card|
             if card.value.class == Integer
@@ -118,14 +118,14 @@ class Hand
         end
     end
 
-    def card_count
+    def card_count # Creates a hash of how many of each card
         cnt = {}
         @cards.each { |card| cnt[card.value] = 0 }
         @cards.each { |card| cnt[card.value] += 1 }
         return cnt
     end
 
-    def high_card
+    def high_card #Determines the card with highest value on hand
         if @values.count > 5
             return :ace_high
         end
@@ -144,39 +144,39 @@ class Hand
     end
 
 
-    def pair?
+    def pair? # Checks the hand for pairs
         card_count.any? { |card, count| count == 2 }
     end
 
-    def two_pair?
+    def two_pair? # Checks the hand for 2 pairs
         card_count.select { |card, count| count == 2 }.count == 2
     end
 
-    def three_of_kind?
+    def three_of_kind? # Checks the hand for 3 of a kind
         card_count.any? { |card, count| count == 3 }
     end
 
-    def straight?
+    def straight? # Checks the hand for a straight
         @values.sort[-1] - @values.sort[0] == 4
     end
 
-    def flush?
+    def flush? # Checks the hand for a flush
         @cards.all? { |card| card.suit == @cards[0].suit }
     end
 
-    def full_house?
+    def full_house? # Checks the hand for a full house
         return three_of_kind? && pair?
     end
 
-    def straight_flush?
+    def straight_flush? # Checks the hand for a straight flush
         return straight? && flush?
     end
 
-    def four_of_a_kind?
+    def four_of_a_kind? # Checks the hand for 4 of a kind
         card_count.any? { |card, count| count == 4 }
     end
 
-    def rank
+    def rank # Analyzes the over all value of the hand as a whole
         sort_vals
         if four_of_a_kind?
             return :four_of_a_kind
@@ -199,7 +199,7 @@ class Hand
         end
     end
 
-    def weight
+    def weight # Assigns a numerical value to the hand by rank
         tie_breaker = @values.sort.last
         if rank == :pair
             return 15 + tie_breaker
@@ -241,11 +241,11 @@ class Player
 
     attr_accessor :hand, :chips, :players_bet, :rating, :name 
 
-    def initialize(chips, name)
-        @chips, @name, @hand, @players_bet = chips, name, Hand.new, 0
+    def initialize(chips, name, hand = Hand.new) # Creates the player to hold a hand and chips
+        @chips, @name, @hand, @players_bet = chips, name, hand, 0
     end
 
-    def see_hand
+    def see_hand # Outputs a display of the hand
         c = []
         @hand.cards.each do |card|
             if card.suit == :hearts
@@ -261,18 +261,18 @@ class Player
         c.sort.each { |card| print card + " " }
     end
 
-    def fold
+    def fold # forfeits the hand of cards and right to play to the showdown
         @hand.cards = []
     end
 
-    def has_hand?
+    def has_hand? # Determines if the players has folded
         return false if @hand.cards.empty?
         return true
     end
 
-    def antie_up?
+    def antie_up? # Asks the user if they want to play the hand they were dealt
         system 'clear'
-        see_hand
+        see_hand # shows hand to player
         puts "\n#{@name} you have #{@chips} chips."
         puts "15 chips to play your hand."
         puts "Do you want to play? (y/n)"
@@ -286,7 +286,7 @@ class Player
         end
     end
 
-    def trade?
+    def trade? # Asks the player if they want to exchange any cards
         system 'clear'
         puts "You can trade up to 3 cards..."
         puts "Don't want to trade? Press ENTER"
@@ -332,15 +332,15 @@ class Player
         return n
     end
 
-    def action?(current_bet)
+    def action?(current_bet) # Asks each player what move they will make
         @bet = 0
         system 'clear'
         puts "#{@name}'s bet is #{@players_bet}"
         puts "You have #{@chips} chips"
         puts "Would you like to..."
-        if current_bet == 0
+        if current_bet == 0 # If no bets have been placed
             puts "fold, check, or bet?"
-        elsif current_bet > 0
+        elsif current_bet > 0 # If a bet has already been wagered
             puts "fold, call, or raise?"
             puts "Current bet is #{current_bet}"
         end
@@ -354,7 +354,7 @@ class Player
     end
 
 
-    def bet(min)
+    def bet(min) # Asks the players for an amount to bet and moves chips
         system 'clear'
         puts "What would you like to bet from #{min} to #{@chips}?"
         @players_bet = gets.chomp.to_i
@@ -365,15 +365,15 @@ class Player
         end
     end
 
-    def called(current_bet)
-        if current_bet <= @chips
+    def called(current_bet) # Moves chips into pot to match a previous bet
+        if current_bet <= @chips # If player has enough chips to cover the bet
             @chips -= current_bet
             @players_bet = current_bet
             puts "#{@name}, called the bet"
             puts "and has #{@chips} chips remaining."
             sleep(3)
             return @players_bet
-        elsif current_bet > @chips
+        elsif current_bet > @chips # If player goes all in to cover the bet
             @players_bet = current_bet
             b = @chips
             @chips = 0
@@ -385,11 +385,11 @@ class Player
         
     end
 
-    def strength_rating
+    def strength_rating # Assigns the overall value of the hand to a rating variable
         @rating = @hand.weight
     end
 
-    def award(pot)
+    def award(pot) # Adds a won pot to total chips
         @chips += pot
     end
 
@@ -405,14 +405,14 @@ class Game
     attr_reader :deck, :players, :pot, :bet, :cards
 
     def initialize(num_players)
-        @players = []
-        @deck = Deck.new
-        make_players(num_players)
-        @pot = 0
-        @bet = 0 
+        @players = [] # Table for the players
+        @deck = Deck.new # New deck of cards
+        make_players(num_players) # Seats players at table
+        @pot = 0 # Empty pot
+        @bet = 0 # No bets yet
     end
 
-    def make_players(n)
+    def make_players(n) # Initializes generic players to fill the requested number of seats
         p = 1
         until n == 0
             player_name = "Player" + p.to_s
@@ -422,22 +422,22 @@ class Game
         end
     end
 
-    def shuffle_cards
+    def shuffle_cards # Shuffles the deck of cards for randomness
         @cards = @deck.cards.values.flatten.shuffle
     end
 
-    def deal_cards
+    def deal_cards # Appropriates 5 cards to each player
         @players.each { |player| player.hand.cards = @cards.pop(5) }
     end
 
-    def antie_up
+    def antie_up # Asks each player if they want to play the hand they were dealt
         @players.each do |player|
             player.players_bet = 0
-            if player.antie_up?
+            if player.antie_up? # Antie costs 15 for each player
                 if player.chips >= 15
                     player.chips -= 15
                     @pot += 15
-                elsif player.chips < 15
+                elsif player.chips < 15 # Players can go all in on the antie
                     @pot += player.chips
                     player.chips = 0
                 end
@@ -445,30 +445,33 @@ class Game
         end
     end
 
-    def trade_cards
+    def trade_cards # Asks each player if they want to trade up to 3 cards
         @players.each do |player|
             trades = player.trade?
             player.hand.cards.push @cards.pop trades if trades > 0
             player.hand.cards.flatten!
             puts "Your new hand is..."
-            player.see_hand
+            player.see_hand # Shows the player their new hand
             sleep(5)
         end
     end
 
-    def option
+    def option # Asks each player what action they will take this round
         players_in_game.each do |player|
             if @bet == 0 || player.players_bet != @bet
                 decision = player.action?(@bet)
                 if decision == "bet" || decision == "raise"
-                    n = player.bet(@bet)
+                    # Increases chips in pot and puts further action on players
+                    n = player.bet(@bet) 
                     if n > @bet
                         @bet = n
                         @pot += n
                     end
                 elsif decision == "call"
+                    # Calls the current bet to move forward
                     @pot += player.called(@bet)
                 elsif decision == "fold"
+                    # Forfeits this hand and all chips in pot
                     player.fold
                 end
                 if number_of_players == 1
@@ -476,28 +479,27 @@ class Game
                 end
             end
         end
-        return if settled?
+        return if settled? # Loops until all bets are settled
         option
     end
 
-    def settled?
-        # equal_bets = players_in_game.delete_if { |player| player.chips == 0 }
+    def settled? # Verifies all bets are equal or players are all in
         players_in_game.all? { |player| player.players_bet == @bet if player.has_hand? }
     end
 
-    def show_cards
+    def show_cards # Reveals the cards of the players remaining in the hand at the end
         @players.each do |player|
             puts "#{player.name} has a #{player.hand.rank}" if player.has_hand?
         end
             puts "#{determine_winner.name} has won this hand."
     end
 
-    def new_dealer
+    def new_dealer # Moves the dealer chip and the player under the gun
         new_dealer = @players.shift
         @players.push new_dealer
     end
 
-    def determine_winner
+    def determine_winner # Evaluates each players cards to pick the winner
         return players_in_game.first if number_of_players == 1
         winner = []
         @players.each do |player|
@@ -512,56 +514,58 @@ class Game
         return winner.first
     end
 
-    def award_pot(player)
+    def award_pot(player) # Gives all the chips in the pot to a player
         puts "#{player.name} has won #{@pot} chips"
         sleep(5)
         player.award(@pot)
         @pot = 0
     end
 
-    def remove_losers
+    def remove_losers # Removes players who have lost all of their chips
         @players.delete_if { |player| player.chips == 0 }
     end
 
-    def clear_cards
+    def clear_cards # Removes a players cards after the hand is played
         @players.each { |player| player.hand.cards = [] }
     end
 
-    def players_in_game
+    def players_in_game # A list of all players currently in the hand
         @players.select { |player| player.has_hand? }
     end
 
-    def number_of_players
+    def number_of_players # The number of players currently in the hand
         players_in_game.count
     end
 
-    def play
+    def play # The loop to facilitate game play for each hand until 1 player remains
         until @players.count == 1
-            @pot = 0
-            @bet = 0
-            shuffle_cards
+            @pot = 0 # Clears the pot
+            @bet = 0 # Clears the current bet
+            shuffle_cards 
             deal_cards
             antie_up
+            # If only 1 player decided to antie up
             if number_of_players == 1
                 award_pot(players_in_game.first) 
             else 
-                trade_cards
-            #debugger
-                option
+                trade_cards # Take player trades
+                option # Get player actions until settled
+                # If all players but 1 fold during action
                 if number_of_players == 1
                     award_pot(players_in_game.first)
-                else
+                else # All remaining players show cards
                     show_cards
+                    # The winner is evaluated and awarded the pot
                     award_pot(determine_winner)
                 end
             end
-            remove_losers
-            clear_cards
-            new_dealer
+            remove_losers # Players get up from the table if they are out of chips
+            clear_cards # Players give cards to dealer
+            new_dealer # Dealer chip is moved
         end
     end
 
 end
-
+# A sample game set up with 2 players.
 game = Game.new(2)
 game.play
