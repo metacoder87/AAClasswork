dde9aa5bbe46e965deda204444f35b7bd000fe55
metacoder87 <gary.miller@gmx.com>
2021-10-23 13:40:07 -0400

    Adds sorting helper methods

--- gary_miller_anagrams/anagrams.rb
+++ gary_miller_anagrams/anagrams.rb
@@ -116,6 +116,35 @@
 
 # What is the time complexity of this solution? 
 # Is it better or worse than #second_anagram??
+      # Merge Sort: O(n*lg(n))
+    def merge_sort (string, &prc)
+        return string if string.length <= 1
+        array = string.split("")
+        mid_idx = string.length / 2
+        merge(
+            merge_sort(array.take(mid_idx), &prc),
+            merge_sort(array.drop(mid_idx), &prc),
+            &prc
+            )
+    end
+
+    # NB: In Ruby, shift is an O(1) operation. This is not true of all languages.
+    def merge(left, right, &prc)
+        merged_array = []
+        prc = Proc.new { |fish1, fish2| fish1.length <=> fish2.length } unless block_given?
+        until left.empty? || right.empty?
+            case prc.call(left.first, right.first)
+                when -1
+                    merged_array << left.shift
+                when 0
+                    merged_array << left.shift
+                when 1
+                    merged_array << right.shift
+            end
+        end
+
+        merged_array + left + right
+    end
 
 # Phase IV:
 # Write one more method #fourth_anagram?. 
