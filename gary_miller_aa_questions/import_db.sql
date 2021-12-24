5d6ba8159c1649aadf05fd08eb136fa4e0a463f2
metacoder87 <gary.miller@gmx.com>
2021-12-24 00:09:09 -0500

    Adds questions table

--- gary_miller_aa_questions/import_db.sql
+++ gary_miller_aa_questions/import_db.sql
@@ -5,3 +5,14 @@ CREATE TABLE users (
     fname CHAR(30) NOT NULL,
     lname CHAR(30) NOT NULL
 );
+
+--Questions
+
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY,
+    questioner_id INTEGER NOT NULL,
+    title CHAR(50) NOT NULL,
+    body CHAR(500) NOT NULL,
+    
+    FOREIGN KEY (questioner_id) REFERENCES users(id)
+);
