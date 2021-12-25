f45895e3e33fc8d7156ae92ac7e4d4e554f7857a
metacoder87 <gary.miller@gmx.com>
2021-12-25 09:58:09 -0500

    Adds Question find by id

--- gary_miller_aa_questions/questions_database.rb
+++ gary_miller_aa_questions/questions_database.rb
@@ -41,6 +41,20 @@ end
 
 class Question
 
+    self.find_by_id(id)
+        question = QuestionDatabase.instance.execute(<<-SQL, id)
+            SELECT
+                *
+            FROM
+                questions
+            WHERE
+                id = ?
+        SQL
+        return nil unless question.length > 0
+
+        Question.new(question.first)
+    end
+
 end
 
 class QuestionFollow
