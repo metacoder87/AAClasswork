ee6acf3fadd187997862ee427e2978ea9af91924
metacoder87 <gary.miller@gmx.com>
2021-12-25 09:59:14 -0500

    Adds Reply find by id

--- gary_miller_aa_questions/questions_database.rb
+++ gary_miller_aa_questions/questions_database.rb
@@ -75,6 +75,18 @@ end
 
 class Reply
 
+    self.find_by_id(id)
+        reply = QuestionDatabase.instance.execute(<<-SQL, id)
+            SELECT
+                *
+            FROM
+                replies
+            WHERE
+                id = ?
+        SQL
+        return nil unless reply.length > 0
+    end
+
 end
 
 class QuestionLike
