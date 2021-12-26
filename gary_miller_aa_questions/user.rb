d279557ca4de789313086dd4b13f7ee36e98f348
metacoder87 <gary.miller@gmx.com>
2021-12-26 13:35:30 -0500

    Adds testable user class file

---
+++ gary_miller_aa_questions/user.rb
@@ -0,0 +1,48 @@
+
+class User
+
+    def self.all
+        user = QuestionsDatabase.instance.execute(<<-SQL)
+            SELECT
+                *
+            FROM
+                users
+            SQL
+    end
+
+    def self.find_by_id(id)
+        user = QuestionsDatabase.instance.execute(<<-SQL, id)
+            SELECT
+                *
+            FROM
+                users
+            WHERE
+                id = ?
+        SQL
+        return nil unless user.length > 0
+
+        User.new(user.first)
+    end
+
+    def self.find_by_name(fname, lname)
+        hashed = { fname: fname, lname: lname }
+        user = QuestionsDatabase.instance.execute(<<-SQL, hashed)
+            SELECT
+                *
+            FROM
+                users
+            WHERE
+                users.fname = :fname AND users.lname = :lname
+        SQL
+        return nil unless user.length > 0
+
+        User.new(user.first)
+    end
+
+    attr_reader :id
+    attr_accessor :fname, :lname
+
+    def initialize(options = {})
+        @id, @fname, @lname = options.values_at('id', 'fname', 'lname')
+    end
+end
