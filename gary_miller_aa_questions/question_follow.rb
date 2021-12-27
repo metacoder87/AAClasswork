e714766c908dfdf665f2b90b98812e0e2d1f5719
metacoder87 <gary.miller@gmx.com>
2021-12-26 22:53:46 -0500

    Debugs QuestionFollow class

--- gary_miller_aa_questions/question_follow.rb
+++ gary_miller_aa_questions/question_follow.rb
@@ -4,16 +4,61 @@ require_relative 'question'
 
 class QuestionFollow
 
+    def self.all
+        follow = QuestionsDatabase.instance.execute(<<-SQL)
+            SELECT
+                *
+            FROM
+                question_follows
+            SQL
+    end
+
     def self.find_by_id(id)
-        qfollow = QuestionsDatabase.instance.execute(<<-SQL, id)
+        hashed = { id: id }
+        follow = QuestionsDatabase.instance.execute(<<-SQL, hashed)
             SELECT
                 *
             FROM
                 question_follows
             WHERE
-                id = ?
+                question_follows.id = :id
         SQL
-        return nil unless qfollow.length > 0
+        follow.map { |data| QuestionFollow.new(data) }
+    end
+
+    def self.find_by_follower_id(follower_id)
+        hashed = { follower_id: follower_id }
+
+        follow = QuestionsDatabase.instance.execute(<<-SQL, hashed)
+            SELECT
+                *
+            FROM
+                question_follows
+            WHERE
+                question_follows.follower_id = :follower_id
+            SQL
+
+        follow.map { |data| QuestionFollow.new(data) }
+    end
+
+    def self.find_by_question_id(question_id)
+        hashed = { question_id: question_id }
+
+        follow = QuestionsDatabase.instance.execute(<<-SQL, hashed)
+            SELECT
+                *
+            FROM
+                question_follows
+            WHERE
+                question_follows.question_id = :question_id
+            SQL
+
+        follow.map { |data| QuestionFollow.new(data) }
+    end
+
+    def initialize(options)
+        @id, @follower_id, @question_id =
+            options.values_at('id', 'follower_id', 'question_id')
     end
 
 end
