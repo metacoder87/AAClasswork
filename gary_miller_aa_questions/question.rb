2ccd4e061271a49d63877a2d1e057f0c0a37ba99
metacoder87 <gary.miller@gmx.com>
2021-12-28 15:10:09 -0500

    Adds Question save

--- gary_miller_aa_questions/question.rb
+++ gary_miller_aa_questions/question.rb
@@ -64,6 +64,31 @@ class Question
         options.values_at('id', 'questioner_id', 'title', 'body')
     end
 
+    def save
+        hashed = { questioner_id: questioner_id, title: title, body: body }
+
+        if @id
+            QuestionsDatabase.execute(<<-SQL, hashed.merge( { id: id } ))
+                UPDATE
+                    users
+                SET
+                    questioner_id = :questioner_id, title = :title, body = :body
+                WHERE
+                    questions.id = :id
+            SQL
+        else
+            QuestionsDatabase.execute(<<-SQL, hashed)
+                INSERT INTO
+                    users (questioner_id, title, body)
+                VALUES
+                    (:questioner_id, :title, :body)
+            SQL
+
+            @id = QuestionsDatabase.last_insert_row_id
+        end
+        self
+    end
+
     def author
         User.find_by_id(questioner_id)
     end
