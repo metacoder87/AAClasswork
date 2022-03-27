96db76c2369a64cce513e686de7dc29d6827747d
metacoder87 <gary.miller@gmx.com>
2022-03-27 01:47:19 -0400

    Updates the schema

--- gary_miller_polls_app/db/schema.rb
+++ gary_miller_polls_app/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220327053902) do
+ActiveRecord::Schema.define(version: 20220327054430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answer_choices", force: :cascade do |t|
+    t.string "text", null: false
+    t.integer "question_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answer_choices_on_question_id"
+  end
 
   create_table "polls", force: :cascade do |t|
     t.string "title", null: false
