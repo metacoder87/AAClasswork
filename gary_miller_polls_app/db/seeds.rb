bf629c24c0400d38bfcf2ee9da343597dc57dcd3
metacoder87 <gary.miller@gmx.com>
2022-03-27 17:11:10 -0400

    Fills in some poll seeds

--- gary_miller_polls_app/db/seeds.rb
+++ gary_miller_polls_app/db/seeds.rb
@@ -12,7 +12,71 @@ ActiveRecord::Base.transaction do
     AnswerChoice.destroy_all
     Response.destroy_all
 
-    user1 = User.create!(username: 'Dantelion')
-    user2 = User.create!(username: 'Feynmen')
+    user1 = User.create!(
+        username: 'Dantelion'
+    )
+    user2 = User.create!(
+        username: 'Feynmen'
+    )
+
+    poll1 = Poll.create!(
+        title: 'Quantum Limits', 
+        author: user2
+    )
+    poll2 = Poll.create!(
+        title: 'Demonic Entanglement', 
+        author: user1
+    )
+
+    q1 = Question.create!(
+        text: 'What is the smallest scale?', 
+        poll: poll1
+    )
+    ac1 = AnswerChoice.create!(
+        text: 'Bathroom', 
+        question: q1
+    )
+    ac2 = AnswerChoice.create!(
+        text: 'Nano', 
+        question: q1
+    )
+    ac3 = AnswerChoice.create!(
+        text: 'Planck', 
+        question: q1
+    )
+
+    q2 = Question.create!(
+        text: 'The equation which gives the mathematical 
+                apparatus for the description of quantum 
+                mechanical systems was formulated by, and 
+                is named after, which Austrian physicist?', 
+        poll: poll1
+    )
+    ac4 = AnswerChoice.create!(
+        text: 'Gerhard Schröder', 
+        question: q2
+    )
+    ac5 = AnswerChoice.create!(
+        text: 'Arnold Schwarzenegger', 
+        question: q2
+    )
+    ac6 = AnswerChoice.create!(
+        text: 'James R. Schlesinger', 
+        question: q2
+    )
+    ac7 = AnswerChoice.create!(
+        text: 'Erwin Schrödinger', 
+        question: q2
+    )
+
+    r1 = Response.create!(
+        respondent: user1,
+        answer_choice: ac3
+    )
+
+    r2 = Response.create!(
+        respondent: user1,
+        answer_choice: ac7
+    )
 
 end
