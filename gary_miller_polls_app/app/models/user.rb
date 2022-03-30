9485cf103528af93bd133b17bac20d06ecd6cb46
metacoder87 <gary.miller@gmx.com>
2022-03-30 01:58:57 -0400

    Adds completed polls using SQL

--- gary_miller_polls_app/app/models/user.rb
+++ gary_miller_polls_app/app/models/user.rb
@@ -11,10 +11,28 @@ class User < ApplicationRecord
         foreign_key: :respondent_id,
         class_name: 'Response'
 
-    def incomplete_polls
-        polls_with_completion_counts
-            .having('COUNT(DISTINCT questions.id) > COUNT(responses.id)')
-            .having('COUNT(responses.id) > 0')
+    def completed_polls_sql
+        Poll.find_by_sql(<<-SQL)
+            SELECT
+                polls.*
+            FROM
+                polls
+            JOIN
+                questions ON polls.id = questions.poll_id
+            JOIN
+                answer_choices ON questions.id = answer_choices.question_id
+            LEFT OUTER JOIN (
+                SELECT
+                    *
+                FROM
+                    responses
+                WHERE
+                    respondent_id = #{self.id}
+            ) AS responses ON answer_choices.id = responses.answer_choice_id
+            GROUP BY
+                polls.id
+            HAVING
+                COUNT(DISTINCT questions.id) = COUNT(responses.*)
+        SQL
     end
-
 end
