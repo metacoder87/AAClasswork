c2fcb91928aaba622f251f564f1b215b34468ad1
metacoder87 <gary.miller@gmx.com>
2022-03-30 01:59:16 -0400

    Adds active record completed polls

--- gary_miller_polls_app/app/models/user.rb
+++ gary_miller_polls_app/app/models/user.rb
@@ -35,4 +35,35 @@ class User < ApplicationRecord
                 COUNT(DISTINCT questions.id) = COUNT(responses.*)
         SQL
     end
+
+    def completed_polls
+        polls_with_completion_counts
+            .having('COUNT(DISTINCT questions.id) = COUNT(responses.id)')
+    end
+
+    def incomplete_polls
+        polls_with_completion_counts
+            .having('COUNT(DISTINCT questions.id) > COUNT(responses.id)')
+            .having('COUNT(responses.id) > 0')
+    end
+
+private
+
+    def polls_with_completion_counts
+        joins_sql = <<-SQL
+            LEFT OUTER JOIN (
+                SELECT
+                    *
+                FROM
+                    responses
+                WHERE
+                    respondent_id = #{self.id}
+            ) AS responses ON answer_choices.id = responses.answer_choice_id
+        SQL
+
+        Poll.joins(questions: :answer_choices)
+                .joins(joins_sql)
+                .group('polls.id')
+    end
+    
 end
