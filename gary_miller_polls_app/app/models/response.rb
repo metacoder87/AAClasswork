cf0fab60c12d22c48beb020510a5d9ffba47a39c
metacoder87 <gary.miller@gmx.com>
2022-03-30 00:42:43 -0400

    Adds sibling responses

--- gary_miller_polls_app/app/models/response.rb
+++ gary_miller_polls_app/app/models/response.rb
@@ -12,4 +12,32 @@ class Response < ApplicationRecord
     has_one :question,
         through: :answer_choice,
         source: :question
+
+    validate :not_duplicate_response, unless: -> { answer_choice.nil? }
+
+    validate :respondent_is_not_poll_author, unless: -> { answer_choice.nil? }
+
+    def sibling_responses
+        ids = { answer_choice_id: self.answer_choice_id, id: self.id }
+        Response.find_by_sql([<<-SQL, ids])
+        SELECT 
+            responses.*
+        FROM (
+            SELECT
+                questions.*
+            FROM
+                questions
+            JOIN
+                answer_choices ON questions.id = answer_choices.question_id
+            WHERE
+                answer_choices.id = :answer_choice_id
+        )   AS questions
+        JOIN
+            answer_choices ON questions.id = answer_choices.question_id
+        JOIN
+            responses ON answer_choices.id = responses.answer_choice_id
+        WHERE
+            (:id IS NULL) OR (responses.id != :id)
+        SQL
+    end
 end
