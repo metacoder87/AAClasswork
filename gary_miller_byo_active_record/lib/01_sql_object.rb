9ba24b6c14601684a3c432187603ff5ba0bffded
metacoder87 <gary.miller@gmx.com>
2022-04-04 02:09:07 -0400

    SQL object WIP

--- gary_miller_byo_active_record/lib/01_sql_object.rb
+++ gary_miller_byo_active_record/lib/01_sql_object.rb
@@ -6,21 +6,49 @@ require 'active_support/inflector'
 class SQLObject
   def self.columns
     # ...
+    return @columns if @columns
+    clmns = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      LIMIT 0
+    SQL
+    clmns.map!(&:to_sym)
+    @columns = clmns
   end
 
   def self.finalize!
+    self.columns.each do |column|
+      define_method(column) do
+        self.attributes[column]
+      end
+      define_method("#{column}=") do |value|
+        self.attributes[column] = value
+      end
+    end
   end
 
   def self.table_name=(table_name)
     # ...
+    @table_name = table_name
   end
 
   def self.table_name
     # ...
+    @table_name || self.name.underscore.pluralize
   end
 
   def self.all
     # ...
+    everything = DBConnection.execute(<<-SQL)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+    SQL
+
+    parse_all(everything)
   end
 
   def self.parse_all(results)
@@ -33,14 +61,24 @@ class SQLObject
 
   def initialize(params = {})
     # ...
+    params.each do |name, val|
+      name = name.to_sym
+      if self.class.columns.include?(name)
+        self.send("#{name}=", val)
+      else
+        raise "name not found '#{name}'"
+      end
+    end
   end
 
   def attributes
     # ...
+    @attributes ||= {}
   end
 
   def attribute_values
     # ...
+    self.class.columns.map { |attr| self.send(attr) }
   end
 
   def insert
