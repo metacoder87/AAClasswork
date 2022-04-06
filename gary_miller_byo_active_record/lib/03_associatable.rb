5091dec75c480450d058223bf4fde82bde28acb3
metacoder87 <gary.miller@gmx.com>
2022-04-06 01:08:58 -0400

    Adds BelongsToOptions

--- gary_miller_byo_active_record/lib/03_associatable.rb
+++ gary_miller_byo_active_record/lib/03_associatable.rb
@@ -21,6 +21,15 @@ end
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
     # ...
+    defaults = {
+      :primary_key => :id,
+      :foreign_key => "#{name}_id".to_sym,
+      :class_name => name.to_s.camelcase
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
