6ae17d7a511f542867621fbb463eac1d6b66fef7
metacoder87 <gary.miller@gmx.com>
2022-04-07 15:45:03 -0400

    Adds has_many, assoc_options, sql object mix in

--- gary_miller_byo_active_record/lib/03_associatable.rb
+++ gary_miller_byo_active_record/lib/03_associatable.rb
@@ -68,10 +68,23 @@ module Associatable
 
   def has_many(name, options = {})
     # ...
+    self.assoc_options[name] = 
+      HasManyOptions.new(name, self.name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+
+      key_val = self.send(options.primary_key)
+      options
+        .model_class
+        .where(options.foreign_key => key_val)
+    end
   end
 
   def assoc_options
     # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+    return @assoc_options
   end
 end
 
